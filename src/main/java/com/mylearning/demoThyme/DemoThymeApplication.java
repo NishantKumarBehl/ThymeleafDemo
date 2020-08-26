f6bedbe2c6d573afe543c9246ae9f4b6fd01155b
Nishant kumar  Behl <nrbehl@gmail.com>
2020-08-26 12:15:21 +0530

    instructions for component scan added in main class

--- src/main/java/com/mylearning/demoThyme/DemoThymeApplication.java
+++ src/main/java/com/mylearning/demoThyme/DemoThymeApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+// Component Scan not required but since i didn't follow the package scope, its a must
 @SpringBootApplication
 @ComponentScan(basePackages={"com.mylearning"})
 public class DemoThymeApplication {
